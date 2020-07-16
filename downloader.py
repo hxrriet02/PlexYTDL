87d045b3957145a96d32ec52e241082dc39d98b4
Harry <harrykharrison@hotmail.co.uk>
2020-07-17 00:37:59 +0100

    Added separate file for downloaders.

---
+++ downloader.py
@@ -0,0 +1,124 @@
+import json, urllib, os, subprocess, youtube_dl
+
+# Variable
+AudioTypes = ["webm", "m4a"]
+VideoTypes = ["mp4", "webm"]
+
+def artwork(ChannelID, ChannelName, settings, UseID = True):
+    api_key = settings["api_key"]
+    if UseID == True:
+        query = f"https://www.googleapis.com/youtube/v3/channels/?part=snippet,statistics,brandingSettings&key={api_key}&id={ChannelID}"
+    else:
+        query = f"https://www.googleapis.com/youtube/v3/channels/?part=snippet,statistics,brandingSettings&key={api_key}&forUsername={ChannelID}"
+
+    with urllib.request.urlopen(query) as request:
+        rawJson = json.load(request)["items"][0]
+
+    # Download Logo
+    logo = rawJson["snippet"]["thumbnails"]["high"]["url"]
+    image(logo, f'{settings["output_dir"]}/{ChannelName}/poster.jpg')
+    # Download background
+    background = rawJson["brandingSettings"]["image"]["bannerTvImageUrl"]
+    image(background, f'{settings["output_dir"]}/{ChannelName}/background.jpg')
+    # Download banner
+    banner = rawJson["brandingSettings"]["image"]["bannerTabletExtraHdImageUrl"]
+    image(banner, f'{settings["output_dir"]}/{ChannelName}/banner.jpg')
+
+def image(url, output):
+    urllib.request.urlretrieve(url, output)
+
+def videos(settings):
+    with open("videos.json") as f:
+        AllVideos = json.loads(f.read())
+
+    # Loops through each video in videos.json
+    for video in AllVideos:
+        # Get needed information
+        VideoID = video["video_id"]
+        VideoChannel = video["channel_name"]
+        VideoReleaseDate = video["video_release_date"]
+
+        TempDir = settings["temp_dir"]
+        TempPath = f"{TempDir}/{VideoChannel}/{VideoReleaseDate} [{VideoReleaseDate}]"
+        OutputDir = settings["output_dir"]
+        OutputPath = f"{OutputDir}/{VideoChannel}/{VideoReleaseDate} [{VideoReleaseDate}]"
+
+        # Download logo, background and banner
+        artwork(video["channel_id"], VideoChannel, settings)
+
+        # Download thumbnail
+        if not os.path.exists(OutputPath + ".jpg") and settings["download_thumbnails"] == True: # Checks if the thumbnail needs to be downloaded
+            try:
+                os.makedirs(f"{OutputDir}/{VideoChannel}/")
+            except Exception:
+                pass
+
+            urllib.request.urlretrieve(video["video_thumbnail_url"], OutputPath + ".jpg")
+
+        # Download subtitles
+        if settings["download_subtitles"] == True:
+            SubtitleOptions = {
+                "writesubtitles": True,
+                "skip_download": True,
+                "allsubtitles": True,
+                "outtmpl": f"{OutputPath}.%(ext)s"
+            }
+
+            with youtube_dl.YoutubeDL(SubtitleOptions) as subs:
+                subs.download([f"https://www.youtube.com/watch?v={VideoID}"])
+
+        ##############################
+        #                            #
+        #       Download video       #
+        #                            #
+        ##############################
+
+        if not os.path.exists(OutputPath + ".mp4"):
+            # Video only
+            VideoOptions = {
+                "format": "bestvideo",
+                "outtmpl": f"{TempPath} (video).%(ext)s"
+            }
+
+            with youtube_dl.YoutubeDL(VideoOptions) as ytdlv:
+                ytdlv.download([f"https://www.youtube.com/watch?v={VideoID}"])
+
+            # Audio only
+            AudioOptions = {
+                "format": "bestaudio/best",
+                "outtmpl": f"{TempPath} (audio).%(ext)s"
+            }
+
+            with youtube_dl.YoutubeDL(AudioOptions) as ytdla:
+                ytdla.download([f"https://www.youtube.com/watch?v={VideoID}"])
+
+            # Converting audio to mp3 format
+            for ext in AudioTypes:
+                if os.path.exists(f"{TempPath} (audio).mp3"):
+                    pass
+                elif os.path.exists(f"{TempPath} (audio).{ext}"):
+                    input = f"{TempPath} (audio).{ext}"
+                    subprocess.call(f'ffmpeg -i "{input}" -vn -f mp3 "{TempPath} (audio).mp3"')
+
+            AudioOnlyPath = f"{TempPath} (audio).mp3"
+
+            # Finding downloaded video file
+            for ext in VideoTypes:
+                if os.path.exists(f"{TempPath} (video).{ext}"):
+                    VideoOnlyPath = f"{TempPath} (video).{ext}"
+
+            # Combines audio and video into one file
+            subprocess.call(f'ffmpeg -i "{VideoOnlyPath}" -i "{AudioOnlyPath}"' +
+                    f' -metadata title="{video["video_title"]}"' +
+                    f' -metadata author="{video["channel_name"]}"' +
+                    f' -metadata description="{video["video_description"]}"' +
+                    f' -metadata year={video["video_release_date"]}' +
+                    f' -codec copy "{OutputPath}.mp4"')
+
+            # Delete audio and video only files.
+            files = os.listdir(f"{TempDir}")
+
+            for file in files:
+                if ("(audio)" in file) or ("(video)" in file):
+                    if VideoID in file:
+                        os.remove(f"{channel}/{file}")
--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from __future__ import unicode_literals # For YouTube-DL
 import urllib.request, json, os, youtube_dl, ffmpeg, subprocess
 
 # My modules
-import setup, scanner
+import downloader, setup, scanner
 
-# Variable
-AudioTypes = ["webm", "m4a"]
-VideoTypes = ["mp4", "webm"]
 
 def cmd(command):
     subprocess.call(command)
@@ -34,95 +31,4 @@ with open("settings.json", "r") as f:
 scanner.start()
 
 # Download all videos
-with open("videos.json") as f:
-    AllVideos = json.loads(f.read())
-
-# Loops through each video in videos.json
-for video in AllVideos:
-    # Get needed information
-    VideoID = video["video_id"]
-    VideoChannel = video["channel_name"]
-    VideoReleaseDate = video["video_release_date"]
-
-    TempDir = settings["temp_dir"]
-    TempPath = f"{TempDir}/{VideoChannel}/{VideoReleaseDate} [{VideoReleaseDate}]"
-    OutputDir = settings["output_dir"]
-    OutputPath = f"{OutputDir}/{VideoChannel}/{VideoReleaseDate} [{VideoReleaseDate}]"
-
-    # Download thumbnail
-    if not os.path.exists(OutputPath + ".jpg"): # Checks if the thumbnail needs to be downloaded
-        try:
-            os.makedirs(f"{OutputDir}/{VideoChannel}/")
-        except Exception:
-            pass
-
-        urllib.request.urlretrieve(video["video_thumbnail_url"], OutputPath + ".jpg")
-
-    # Download subtitles
-    if settings["download_subtitles"] == True:
-        SubtitleOptions = {
-            "writesubtitles": True,
-            "skip_download": True,
-            "allsubtitles": True,
-            "outtmpl": f"{VideoOnlyPath}.%(ext)s"
-        }
-
-        with youtube_dl.YoutubeDL(SubtitleOptions) as subs:
-            subs.download([f"https://www.youtube.com/watch?v={VideoID}"])
-
-    ##############################
-    #                            #
-    #       Download video       #
-    #                            #
-    ##############################
-
-    if not os.path.exists(OutputPath + ".mp4"):
-        # Video only
-        VideoOptions = {
-            "format": "bestvideo",
-            "outtmpl": f"{TempPath} (video).%(ext)s"
-        }
-
-        with youtube_dl.YoutubeDL(VideoOptions) as ytdlv:
-            ytdlv.download([f"https://www.youtube.com/watch?v={VideoID}"])
-
-        # Audio only
-        AudioOptions = {
-            "format": "bestaudio/best",
-            "outtmpl": f"{TempPath} (audio).%(ext)s"
-        }
-
-        with youtube_dl.YoutubeDL(AudioOptions) as ytdla:
-            ytdla.download([f"https://www.youtube.com/watch?v={VideoID}"])
-
-        # Converting audio to mp3 format
-        for ext in AudioTypes:
-            if os.path.exists(f"{TempPath} (audio).mp3"):
-                pass
-            elif os.path.exists(f"{TempPath} (audio).{ext}"):
-                input = f"{TempPath} (audio).{ext}"
-                cmd(f'ffmpeg -i "{input}" -vn -f mp3 "{TempPath} (audio).mp3"')
-
-        AudioOnlyPath = f"{TempPath} (audio).mp3"
-
-        # Finding downloaded video file
-        for ext in VideoTypes:
-            if os.path.exists(f"{TempPath} (video).{ext}"):
-                VideoOnlyPath = f"{TempPath} (video).{ext}"
-
-        # Combines audio and video into one file
-        cmd(f'ffmpeg -i "{VideoOnlyPath}" -i "{AudioOnlyPath}"' +
-                f' -metadata title="{video["video_title"]}"' +
-                f' -metadata author="{video["channel_name"]}"' +
-                f' -metadata description="{video["video_description"]}"' +
-                f' -metadata year={video["video_release_date"]}' +
-                f' -codec copy "{OutputPath}.mp4"')
-
-        # Delete audio and video only files.
-        files = os.listdir(f"{TempDir}")
-"""
-        for file in files:
-            if ("(audio)" in file) or ("(video)" in file):
-                if VideoID in file:
-                    os.remove(f"{channel}/{file}")
-"""
+downloader.videos(settings)
